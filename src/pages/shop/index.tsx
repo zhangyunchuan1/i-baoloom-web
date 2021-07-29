e93d850cbe653927fc6c53a229e15d0148371aea
zyc <1028580665@qq.com>
2021-07-29 20:52:02 +0800

    feat:==

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,231 +1,191 @@
-import { LinkSharp } from '@material-ui/icons';
-import { now } from '@umijs/deps/compiled/lodash';
 import React, { useState, useEffect } from 'react';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import EditIcon from '@material-ui/icons/Edit';
+import name from '@/images/icons/icons-edit.png';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
-import './index.scss';
-
-let list: { name: string; url: string }[] = [];
-for (let index = 0; index < 20; index++) {
-  list.push({
-    name: `tag-${index}`,
-    url: `url-${index}`,
-  });
-}
-let aa = [
+const tags = [
   {
-    name: `1`,
-    links: [
+    title: 'Flutter资源',
+    child: [
       {
-        name: `1-1`,
+        title: 'Flutter中文网',
+        url: 'https://flutterchina.club/',
+        des:
+          'Flutter是谷歌的移动UI框架，可以快速在iOS和Android上构建高质量的原生用户界面。',
       },
       {
-        name: `1-2`,
+        title: 'Dart学习文档',
+        url: 'https://pub.dev/',
+        des: 'Dart语言基础语法学习文档。',
+      },
+      {
+        title: '《Flutter实战》电子书',
+        url: 'https://book.flutterchina.club/',
+        des:
+          '本书是第一本系统介绍Flutter技术的中文书籍，它是Flutter中文社区发起的开源项目之一，旨在帮助开发者入门，系统地、循序渐进的了解Flutter。',
       },
     ],
   },
   {
-    name: `2`,
-    links: [
+    title: '资源分享',
+    child: [
       {
-        name: `2-1`,
+        title: '前端之巅1',
+        url: 'http://www.youtube.com',
+        des: '本文旨在用最通俗的语言讲述最枯燥的基本知识。',
       },
       {
-        name: `2-2`,
+        title: '前端之巅2',
+        url: 'http://www.youtube.com',
+        des: '本文旨在用最通俗的语言讲述最枯燥的基本知识。',
+      },
+      {
+        title: '前端之巅3',
+        url: 'http://www.youtube.com',
+        des: '本文旨在用最通俗的语言讲述最枯燥的基本知识。',
+      },
+      {
+        title: '前端之巅4',
+        url: 'http://www.youtube.com',
+        des: '本文旨在用最通俗的语言讲述最枯燥的基本知识。',
       },
     ],
   },
   {
-    name: `3`,
-    links: [
+    title: '服务器相关',
+    child: [
       {
-        name: `3-1`,
+        title: '我的博客1',
+        url: 'https://www.cnblogs.com/W-Kr/p/5455862.html',
+        des: '面向新手的Web服务器搭建。',
       },
       {
-        name: `3-2`,
+        title: '我的博客2',
+        url: 'https://www.cnblogs.com/W-Kr/p/5455862.html',
+        des: '面向新手的Web服务器搭建。',
+      },
+      {
+        title: '我的博客3',
+        url: 'https://www.cnblogs.com/W-Kr/p/5455862.html',
+        des: '面向新手的Web服务器搭建。',
+      },
+      {
+        title: '我的博客4',
+        url: 'https://www.cnblogs.com/W-Kr/p/5455862.html',
+        des: '面向新手的Web服务器搭建。',
       },
     ],
   },
 ];
 
+import './index.scss';
+
 const ShopPage: React.FC = (props) => {
-  const [tags, settags] = useState<any>(aa);
-  const [nowMoveDom, setnowMoveDom] = useState<any>(null);
-  const [moveType, setmoveType] = useState(0); //移动类型： 1: 标签 , 2: 文件夹
-  const [nowindex, setnowindex] = useState(0);
-  const [nowind, setnowind] = useState(0);
-  let locationIndex = -1;
-  let locationInd = -1;
+  const [selectMenu, setSelectMenu] = useState(0);
+  //拖动变量
+  const [futureMenuIndex, setFutureMenuIndex] = useState(-1); //拖动标签覆盖到菜单，记录的当前菜单下标
+  const [dragType, setDragType] = useState(-1); //拖动类型：1-菜单、2-标签
 
-  //拖动到覆盖元素的时候，覆盖元素背景色改变为选中状态
-  const changeOverBoxBg = (i: any) => {
-    let tagDoms = document.getElementsByClassName('item');
-    // console.log('tagDoms:', tagDoms);
-    tagDoms.forEach((dom: any, index: any) => {
-      if (i == index) {
-        dom.style.background = '#70a1ff';
-      } else {
-        dom.style.background = '#dfe4ea';
-      }
-    });
+  const handleSelectMenu = (index: number) => {
+    setSelectMenu(index);
   };
 
-  const onDragStartTag = (e: any, index: any, ind: any) => {
-    console.log('onDragStartTag:', e, index, ind);
-    setnowMoveDom(e);
-    setnowindex(index);
-    setnowind(ind);
-    setmoveType(1);
-  };
-
-  const onDragEnterBox = (e: any, index: any) => {
+  //移动覆盖
+  const handleCoverMenu = (e: any, menuIndex: number) => {
     e.preventDefault();
-    console.log('onDragEnterBox:', e, index);
-    if (index != nowindex) {
-      changeOverBoxBg(index);
-    } else {
-      changeOverBoxBg(-1);
-    }
+    setFutureMenuIndex(menuIndex);
   };
 
-  const onDropTag = (e: any, index: any, ind?: any) => {
-    e.preventDefault();
-    locationIndex = index;
-    locationInd = ind;
-    console.log('tag触发-结束拖动', index, ind);
-    let tagDom = document.getElementById(`tag-${index}-${ind}`);
-    if (tagDom) tagDom.style.borderLeft = 'none';
-    // changeOverBoxBg(-1);
-  };
-  //onDrop={(e)=>onDropTag(e, index,ind)}
-  const onDropBox = (e: any, index: any) => {
-    let _tags = JSON.parse(JSON.stringify(tags));
-    e.preventDefault();
-    console.log('box触发-结束拖动', index);
-    changeOverBoxBg(-1);
-    if (moveType == 1) {
-      //移动标签
-      if (index != nowindex) {
-        //移动到其他文件夹
-        //删除当前文件的数据
-        let moveTag = _tags[nowindex].links[nowind];
-        _tags[nowindex].links.splice(nowind, 1);
-        console.log('添加到指定位置：', locationIndex, locationInd);
-        if (locationInd == 0) {
-          _tags[index].links.splice(0, 0, moveTag);
-        } else if (locationInd >= 0) {
-          _tags[index].links.splice(locationInd, 0, moveTag);
-        } else {
-          _tags[index].links.push(moveTag);
-        }
-        console.log(_tags);
-        settags(_tags);
-        locationIndex = -1;
-        locationInd = -1;
-        //在新文件夹下添加标签
-      } else {
-        //在当前文件夹下移动
-        console.log('在当前文件夹下移动s', locationIndex, locationInd);
-        if (
-          nowMoveDom.target.id != `tag-${locationIndex}-${locationInd}` &&
-          locationInd >= 0
-        ) {
-          let moveTag = _tags[nowindex].links[nowind];
-          _tags[index].links.splice(nowind, 1);
-          _tags[index].links.splice(locationInd, 0, moveTag);
-          console.log(_tags);
-          settags(_tags);
-          locationIndex = -1;
-          locationInd = -1;
-        } else {
-          let tagDoms = document.getElementsByClassName('ele');
-          tagDoms.forEach((dom: any, index: any) => {
-            dom.style.borderLeft = 'none';
-          });
-        }
-      }
-    }
+  //在菜单中释放
+  const handleLeaveMenu = (menuIndex: number) => {
+    console.log(`菜单中释放,菜单索引 - ${menuIndex}`);
+    setFutureMenuIndex(-1); //移除菜单覆盖时的背景
   };
 
-  //拖动到覆盖tag元素的时候，覆盖元素背景色改变为选中状态
-  const changeOverTagBg = (i: any, r: any) => {
-    let boxDoms = document.getElementsByClassName('item');
-    let tagDoms = document.getElementsByClassName('ele');
-    console.log('tagDom:', tagDoms);
-    boxDoms.forEach((dom: any, index: any) => {
-      if (i == index) {
-        dom.style.background = '#70a1ff';
-      } else {
-        dom.style.background = '#dfe4ea';
-      }
-    });
-    tagDoms.forEach((dom: any, index: any) => {
-      console.log(dom);
-      if (dom.id == `tag-${i}-${r}`) {
-        dom.style.borderLeft = '5px solid red';
-      } else {
-        dom.style.borderLeft = 'none';
-      }
-    });
-  };
-
-  const onDragEnterTag = (e: any, index: any, ind: any) => {
-    e.preventDefault();
-    console.log('onDragEnterTag:', e, index, ind, nowMoveDom);
-    if (nowMoveDom.target.id != `tag-${index}-${ind}`) {
-      changeOverTagBg(index, ind);
-    } else {
-      let tagDoms = document.getElementsByClassName('ele');
-      tagDoms.forEach((dom: any, index: any) => {
-        dom.style.borderLeft = 'none';
-      });
-    }
-    // if(index != nowindex){
-    //   changeOverTagBg(index, ind);
-    // }else{
-    //   changeOverTagBg(-1, ind);
-    // }
+  const handleLeaveTag = (tagIndex: number) => {
+    console.log(`标签中释放，菜单索引 - ${selectMenu}，标签索引 - ${tagIndex}`);
   };
 
   return (
     <div className="page-shop">
-      {tags.map((item: any, index: any) => {
-        return (
-          <div
-            className="item"
-            key={index}
-            onDragOver={(e) => {
-              e.preventDefault();
-            }}
-            onDragEnter={(e) => {
-              onDragEnterBox(e, index);
-            }}
-            onDrop={(e) => onDropBox(e, index)}
-          >
-            {item.links.map((ele: any, ind: any) => {
-              return (
-                <div
-                  className="ele"
-                  id={`tag-${index}-${ind}`}
-                  key={ind}
-                  draggable="true"
-                  onDragEnter={(e) => {
-                    onDragEnterTag(e, index, ind);
-                  }}
-                  onDragOver={(e) => {
-                    e.preventDefault();
-                  }}
-                  onDragStart={(e) => {
-                    onDragStartTag(e, index, ind);
-                  }}
-                  onDrop={(e) => onDropTag(e, index, ind)}
-                >
-                  {ele.name}
+      <div className="menu-top">
+        <div className="menu-boxs">
+          {tags.map((item, index) => {
+            return (
+              <div
+                className={`box ${
+                  selectMenu == index || futureMenuIndex == index
+                    ? 'selected-box'
+                    : ''
+                }`}
+                key={index}
+                onClick={() => handleSelectMenu(index)}
+                onDragOver={(e) => handleCoverMenu(e, index)}
+                onDrop={() => handleLeaveMenu(index)}
+              >
+                <div className="box-color-name">
+                  <span className="box-color"></span>
+                  <span className="box-name">{item.title}</span>
                 </div>
-              );
-            })}
-          </div>
-        );
-      })}
+                <span className="box-num">{item.child.length}</span>
+                <div className="box-option">
+                  <div className="option-warp">
+                    <AddBoxIcon
+                      style={{
+                        fontSize: 20,
+                        color: 'rgba(99, 110, 114,0.6)',
+                        marginRight: '5px',
+                      }}
+                    />
+                    <EditIcon
+                      style={{ fontSize: 20, color: 'rgba(99, 110, 114,0.6)' }}
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="menu-tags">
+          {tags[selectMenu].child.map((item, index) => {
+            return (
+              <div
+                className="tag"
+                draggable={true}
+                key={index}
+                onDrop={() => handleLeaveTag(index)}
+              >
+                <h3 className="tag-title">{item.title}</h3>
+                <p className="tag-desc">{item.des}</p>
+                <div className="tag-bot">
+                  <div className="tag-bot-left">
+                    <img
+                      className="tag-logo"
+                      src={`https://api.clowntool.cn/getico/?url=${item.url}`}
+                      alt=""
+                    />
+                    <span className="tag-url" title={item.url}>
+                      {item.url}
+                    </span>
+                  </div>
+                  <div className="tag-bot-right">
+                    <img
+                      src={require('../../images/icons/icons-edit.png')}
+                      alt=""
+                    />
+                    <img
+                      src={require('../../images/icons/icons-delete.png')}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
